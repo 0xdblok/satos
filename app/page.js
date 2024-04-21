d59bb4df2971f033473426522188b763338f08b8
0xdblok <dblok0x@gmail.com>
2024-04-21 15:36:42 +0200

    ee

--- app/page.js
+++ app/page.js
@@ -10,7 +10,7 @@ import bleu from "./asset/bleu.png";
 import blank from "./asset/blank.png";
 export default function Home() {
   return (
-    <div className="mx-auto container  px-40 ">
+    <div className="mx-auto container  lg:px-40 ">
       <Nav />
       <Hero />
       <div className="w-full">
